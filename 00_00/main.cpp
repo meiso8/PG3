b243cfe9cde6c668c29f1e2b690149207ce7215e
あ <k024g0101@g.neec.ac.jp>
2025-09-29 12:23:44 +0900

    修正

--- 00_00/main.cpp
+++ 00_00/main.cpp
@@ -1,14 +1,8 @@
 #include<stdio.h>
-//#include<iostream>
-//#include<Windows.h>
 
 int main() {
 
-	//SetConsoleOutputCP(65001);
-	//char str[] = "‚²";
-	//printf("%s",str);
-
-	printf("‚²–³¹‘¿‚µ‚Ä‚¨‚è‚Ü‚·");
+	printf("‚²–³¹‘¿‚µ‚Ä‚¨‚è‚Ü‚·!");
 
 	return 0;
 }
