22b0cee86773f54e46f1ed3ea950fa4b7346308f
あ <k024g0101@g.neec.ac.jp>
2025-10-01 00:09:09 +0900

    完成

---
+++ 02_01/main.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+//抽象化　一括対応
+template <typename Type>Type Min(Type a, Type b) {
+
+    if (a < b) {
+        return static_cast<Type>(a);
+    }
+
+    return  static_cast<Type>(b);
+}
+
+int main() {
+
+    //int
+    printf("%d\n", Min<int>(1, 5));
+    //float
+    printf("%f\n", Min<float>(1.0f, 0.5f));
+    //double
+    printf("%lf\n", Min<double>(20.0, 20.0));
+
+
+    return 0;
+}
