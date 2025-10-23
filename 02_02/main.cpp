632bddb6f9b315f7507024e3e55d19bea54456b6
あ <k024g0101@g.neec.ac.jp>
2025-10-24 05:52:38 +0900

    ok

--- 02_02/main.cpp
+++ 02_02/main.cpp
@@ -3,11 +3,16 @@
 #include<stdint.h>
 #include <string>
 
+//最低賃金
 #define MONEY 1226
+//初項
 #define A0 100
 
 using namespace std;
 
+/// @brief 再帰賃金体系を計算する関数
+/// @param time 時間
+/// @return 再帰賃金体系の合計値
 uint32_t RecursiveMoney(uint32_t time) {
 
     if (time <= 1) {
@@ -18,16 +23,11 @@ uint32_t RecursiveMoney(uint32_t time) {
 
 }
 
+/// @brief 一般賃金体系を計算する関数
+/// @param time 時間
+/// @return 一般賃金体系の合計値
 uint32_t NormalMoney(uint32_t time) {
     return time * MONEY;
-}
-
-void PrintMoney(uint32_t& money, uint32_t& normalMoney) {
-
-    cout << "RecursiveMoney : ";
-    cout << money << endl;
-    cout << "MoneyNormal : ";
-    cout << normalMoney << endl;
 }
 
 int main() {
@@ -37,17 +37,23 @@ int main() {
 
     for (uint32_t time = 1; time < 100; ++time) {
 
-        cout << "time : ";
-        cout << time << endl;
+        //お賃金を計算する
         recursiveMoney = RecursiveMoney(time);
         normalMoney = NormalMoney(time);
-        PrintMoney(recursiveMoney, normalMoney);
+
+        //文字を出力する
+        cout << "time : ";
+        cout << time << endl;
+        cout << "RecursiveMoney : ";
+        cout << recursiveMoney << endl;
+        cout << "MoneyNormal : ";
+        cout << normalMoney << endl;
 
         if (normalMoney <= recursiveMoney) {
+            //上回ったら抜ける
             break;
         }
     }
-
 
     return 0;
 }
